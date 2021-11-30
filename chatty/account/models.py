9d85c9e0c321e5ac05cd52652a782896c0099d26
Shubham Yadav <subhamyadav580@gmail.com>
2021-11-30 19:06:43 +0530

    adding custom auth

--- chatty/account/models.py
+++ chatty/account/models.py
@@ -1,3 +1,57 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+	def create_user(self, email, username, password=None):
+		if not email:
+			raise ValueError('Users must have an email address')
+		if not username:
+			raise ValueError('Users must have a username')
+
+		user = self.model(
+			email=self.normalize_email(email),
+			username=username,
+		)
+
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+	def create_superuser(self, email, username, password):
+		user = self.create_user(
+			email=self.normalize_email(email),
+			password=password,
+			username=username,
+		)
+		user.is_admin = True
+		user.is_staff = True
+		user.is_superuser = True
+		user.save(using=self._db)
+		return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
--- chatty/chat/models.py
+++ chatty/chat/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from account.models import User
+
 class Message(models.Model):
-    username = models.CharField(max_length=255)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.CharField(max_length=255)
     content = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
